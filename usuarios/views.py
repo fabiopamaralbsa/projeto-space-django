8b4e6232e364124d876646a8d2077e5dc90c76db
fabiopamaralbsa <fabio.amaral@bsatech.io>
2025-10-22 14:59:51 -0300

    Exibição das mensagens de erro/sucesso no login e cadastro

--- usuarios/views.py
+++ usuarios/views.py
@@ -3,7 +3,7 @@ from usuarios.forms import LoginForms, CadastroForms
 
 from django.contrib.auth.models import User
 
-from django.contrib import auth
+from django.contrib import auth, messages
 
 def login(request):
     form = LoginForms()
@@ -23,8 +23,10 @@ def login(request):
 
         if usuario is not None:
             auth.login(request, usuario)
+            messages.success(request, f'{nome} logado com sucesso')
             return redirect('index')
         else:
+            messages.error(request, 'Erro ao efetuar login')
             return redirect('login')
 
     return render(request, "usuarios/login.html", {"form": form})
@@ -37,6 +39,7 @@ def cadastro(request):
         
         if form.is_valid():
             if form["senha_1"].value() != form["senha_2"].value():
+                messages.error(request, 'Senhas não coincidem')
                 return redirect('cadastro')
             
             nome = form["nome_cadastro"].value()
@@ -44,6 +47,7 @@ def cadastro(request):
             senha = form["senha_1"].value()
 
             if User.objects.filter(username=nome).exists():
+                messages.error(request, 'Esse usuário já existe.')
                 return redirect('cadastro')
 
             usuario = User.objects.create_user(
@@ -52,6 +56,7 @@ def cadastro(request):
                 password=senha
             )
             usuario.save()
+            messages.success(request, 'Cadastro efetuado com sucesso!')
             return redirect('login')
             
             
